a9bfef2f2b9d2964732a8d43d552b299ecccd299
Alena Lungova <alena.lungova1@gmail.com>
2019-10-22 08:20:02 +0200

    Caesar cipher final (but no way finished) version

---
+++ 04/ceasar_cipher_final.py
@@ -0,0 +1,22 @@
+plaintext = input ("enter text you want to cipher: ")
+key = input ("enter cipher key: ")
+
+while key.isdigit()== False or (int(key) <= 0):
+    print ("Incorrect, enter only number > 0, please!")
+    key = input ("enter cipher key: ")
+
+
+ciphertext = ""
+for i in range(len(plaintext)):
+    letter = plaintext[i]
+    if letter == " ":
+        ciphertext += " "
+    elif letter == letter.lower():
+        ciphertext += chr((ord(letter) + int(key) - 97) % 26 + 97)
+    else:
+        ciphertext += chr((ord(letter) + int(key) - 65) % 26 + 65)
+
+# funguje jen pro znaky abecedy a mezeru, ne pro ostatní znaky
+
+print ("original text: " + plaintext)
+print ("cipher text: " + ciphertext)
