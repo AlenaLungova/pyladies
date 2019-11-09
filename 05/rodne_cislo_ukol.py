6b00e8b425b6bcf10c1237dcb1a8a9ef8507a183
AlenaLungova <56603563+AlenaLungova@users.noreply.github.com>
2019-11-09 18:12:06 +0100

    Delete rodne_cislo_ukol.py

--- 05/rodne_cislo_ukol.py
+++
@@ -1,9 +0,0 @@
-#ukol lekce 5
-rodne_cislo = input("Rodne cislo: ")
-
-jedenacti = rodne_cislo[:6] + rodne_cislo[7:]
-
-if len(rodne_cislo) == 11 and rodne_cislo[:6].isdigit() and rodne_cislo[6] == "/" and rodne_cislo[7:].isdigit() and int(jedenacti) % 11 == 0:
-    print("Číslo zadáno správně.")
-else:
-    print("Někde je chyba.")
